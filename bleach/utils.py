7f4a376858acbe92bb4604ff003a199e8c132268
Greg Guthe <gguthe@mozilla.com>
2020-11-05 10:28:07 -0500

    sanitizer: update sanitize_uri_value for Python 3.9 urlparse
    Use Python 3.9 urlparse scheme parsing behavior for all Python
    versions
    
    Changes:
    
    * add utils._parse_uri_scheme to match Python 3.9 urlparse behavior
    * add utils._is_valid_netloc_and_port with Django URL validator
    * in test_uri_value_allowed_protocols:
      * add test case for implicit http for IP and port with path and fragment
      * add test case for data: scheme
      * add test case for relative path URI
      * test "is not allowed by default" test cases against default
        ALLOWED_PROTOCOLS
      * convert test_invalid_uri_does_not_raise_error into a test case

--- bleach/utils.py
+++ bleach/utils.py
@@ -1,6 +1,9 @@
 from collections import OrderedDict
+import re
 
 import six
+
+from bleach._vendor.django.core.validators import URLValidator
 
 
 def _attr_key(attr):
@@ -40,3 +43,52 @@ def force_unicode(text):
 
     # If not, convert it
     return six.text_type(text, "utf-8", "strict")
+
+
+netloc_port_re = re.compile(
+    "^" + URLValidator.netloc_re + URLValidator.port_re + "$", re.IGNORECASE
+)
+
+
+# Characters valid in scheme names
+scheme_chars = (
+    "abcdefghijklmnopqrstuvwxyz" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "0123456789" "+-."
+)
+
+
+def _is_valid_netloc_and_port(netloc):
+    """
+    Returns the scheme for a URI or None when parsing the URI fails
+
+    :arg str/unicode netloc:
+
+    :returns: bool
+
+    """
+    # The maximum length of a full host name is 253 characters per RFC 1034
+    # section 3.1. It's defined to be 255 bytes or less, but this includes
+    # one byte for the length of the name and one byte for the trailing dot
+    # that's used to indicate absolute names in DNS.
+    netloc = netloc_port_re.match(netloc)
+    return bool(netloc and len(netloc.group(0)) < 254)
+
+
+def _parse_uri_scheme(uri):
+    """
+    Returns the scheme for a URI or None when parsing the URI fails
+
+    :arg str/unicode text:
+
+    :returns: text or None
+
+    """
+    # replicate Python 3.9 urlparse scheme parsing for older Python versions
+    i = uri.find(":")
+    if i > 0:
+        scheme = uri[:i]
+        for c in uri[:i]:
+            if c not in scheme_chars:
+                break
+        return scheme
+
+    return None
