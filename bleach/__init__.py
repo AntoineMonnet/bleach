e1a561246640bcbdc8f09c269f86feef32c73811
Greg Guthe <gguthe@mozilla.com>
2021-01-27 11:02:41 -0500

    Update for v3.4.0 release

--- bleach/__init__.py
+++ bleach/__init__.py
@@ -20,7 +20,7 @@ from bleach.sanitizer import (
 # yyyymmdd
 __releasedate__ = "20210201"
 # x.y.z or x.y.z.dev0 -- semver
-__version__ = "3.3.0"
+__version__ = "3.4.0"
 VERSION = packaging.version.Version(__version__)
 
 
